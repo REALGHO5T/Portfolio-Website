764e53569789d23f4221c34d3f49f33b391ccdae
Ad3an <144132964+Ad3an@users.noreply.github.com>
2025-04-17 12:51:58 +0530

    REDESIGNED

--- script.js
+++ script.js
@@ -1,130 +1,85 @@
-function smoothScroll() {
+const smoothscroll = () => {
   // Initialize Lenis
   const lenis = new Lenis({
     autoRaf: true,
   });
+
   // Listen for the scroll event and log the event data
   lenis.on("scroll", (e) => {
     console.log(e);
   });
-}
+};
+smoothscroll();
 
-function pageview() {
-  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-    anchor.addEventListener("click", function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute("href")).scrollIntoView({
-        behavior: "smooth",
-      });
-    });
-  });
-}
-let menuOpen = document.querySelector(".menu-open");
-let menuClose = document.querySelector(".menu-close");
-let sidebar = document.querySelector(".sidebar");
 
-function Sidebar() {
-  let sl = gsap.timeline();
-  sl.to(".sidebar", {
-    right: 0,
-    duration: 0.4,
-    ease: "power4",
-  });
-  sl.from(".sidebar ul li", {
-    x: 100,
-    duration: 0.4,
-    stagger: 0.1,
-    opacity: 0,
-  });
-  sl.pause();
-  menuOpen.addEventListener("click", function () {
-    sl.play();
-  });
-  menuClose.addEventListener("click", function () {
-    sl.reverse();
-  });
-}
-function PageAnimation() {
+const heroAnimation = () => {
   let hl = gsap.timeline();
-
-  hl.from("header .logo, header ul li", {
-    y: -100,
+  hl.from(".hero-section .hero-content h1", {
+    y: 200,
+    opacity: 0,
+    duration: 1,
+    delay: 1,
+    ease: "power4",
     stagger: 0.1,
-    duration: 0.5,
-    delay: 0.4,
-    ease: Power4,
   });
-  hl.from(".hero-section #text1", {
-    x:-200,
-    duration: 1,
-    ease:"bounce",
-    opacity: 0,
-  });
-  hl.from(".hero-section #text2", {
-    x: 200,
-    duration: 1,
-    ease: "bounce",
-    opacity: 0,
-  },"<");
 
-  let al = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".about-section",
-      start: "-20% 30%",
-      end: "bottom bottom",
-    },
-  });
-  al.from(".about-left", {
-    x: -200,
-    duration: 1,
-    ease: Power4,
-    opacity: 0,
-  });
-  al.from(
-    ".about-right, .about-right p",
+  hl.from(
+    "header p, header .btn",
     {
-      x: 200,
-      duration: 1,
-      ease: Power4,
+      y: -100,
       opacity: 0,
-      stagger: 0.2,
+      duration: 1,
+      ease: "power4",
+      stagger: 0.1,
     },
     "<"
   );
 
-  let vl = gsap.timeline({
+  hl.from(".intro-left, .intro-right", {
+    y: 200,
+    opacity: 0,
+    duration: 1,
+    ease: "power4",
+    stagger: 0.1,
+  });
+};
+
+const projectAnimation = () => {
+  let pl = gsap.timeline({
     scrollTrigger: {
-      trigger: "#case1",
-      start: "-20% 30%",
-      end: "bottom bottom",
+      trigger: ".project-section",
+      start: "top center",
+      end: "bottom top",
+      scrub: false,
     },
   });
 
-  vl.from("#case1 .case-image", {
-    scale:0.5,
-    duration: 1,
-    ease: Power4,
+  pl.from(".project-left, .project-right", {
+    y: 200,
     opacity: 0,
-  });
-  
-  let ml = gsap.timeline({
-    scrollTrigger: {
-      trigger: "#case2",
-      start: "-20% 30%",
-      end: "bottom bottom",
-    },
+    duration: 1,
+    ease: "power2.out",
+    stagger: 0.1,
   });
 
-  ml.from("#case2 .case-image", {
-    scale: 0.5,
-    duration: 1,
-    ease: Power4,
-    opacity: 0,
+   document.querySelectorAll(".project").forEach((project) => {
+    gsap.from(project, {
+      scrollTrigger: {
+        trigger: project,
+        start: "-50% center",
+        end: "bottom top",
+        scrub: false,
+        stagger: 0.3,
+      },
+      y: 200,
+      opacity: 0,
+      duration: 1,
+      ease: "power2.out",
+    });
   });
-  
+};
 
-}
-pageview();
-Sidebar();
-smoothScroll();
-PageAnimation();
+
+
+heroAnimation();
+projectAnimation()
